c1d80fe6bb703513e7c42f681629d035cbfc915c
eleemw <eleemw+codenews@gmail.com>
2020-12-10 15:34:05 -0800

    Added arrays of questions and answers

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?",
+                                "What is 7+7?",
+                                "What is the capital of California?"]
+    let answers: [String] = ["Grapes",
+                                "14",
+                                "Sacramento"]
+    var currentQuestionIndex: Int = 0
+    
+    
+    
     @IBAction func showNextQuestion(sender: AnyObject) {
         
     }
