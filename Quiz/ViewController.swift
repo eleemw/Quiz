bdc6e0d1fb300d30dce2feaafdfa071fcee704e6
eleemw <eleemw+codenews@gmail.com>
2020-12-10 15:24:15 -0800

    Defined action methods: showNextQuestion(_:) and showAnswer(_:)

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -11,5 +11,13 @@ class ViewController: UIViewController {
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        
+    }
 }
 
